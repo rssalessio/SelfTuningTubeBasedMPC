4393d03b56f5c3e7cb2afb84d467a52e8a410918
Alessio Russo <alessior@kth.se>
2022-09-24 15:28:28 +0200

    add images

--- known_B/compute_T.py
+++ known_B/compute_T.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 from typing import Callable, List, Tuple
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import compute_stabilizing_K, compute_joint_spectral_radius, compute_contractive_polytope, build_hypercube
+from utils import compute_stabilizing_K, compute_joint_spectral_radius, compute_contractive_polytope, \
+    build_hypercube, compute_Hc, get_H_problem, MPC_problem, compute_wbar, solve_lyapunov
 import cvxpy as cp
 from hyper_rectangle import HyperRectangle
 from scipy import linalg
+import matplotlib.pyplot as plt
 import time
 np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 A = np.array([
@@ -23,9 +25,11 @@ B = np.array([
 dim_x, dim_u = B.shape
 C = A.flatten()
 
-radius = 3e-1
+radius = 7e-2
 
-parameter_set = HyperRectangle.build_hypercube(C, radius)
+parameter_set = HyperRectangle.build_hypercube(C + 0.01*np.random.uniform(low=-1, high=1, size=((dim_x*2))), radius)
+
+assert parameter_set.contains(A.flatten()), 'True matrix not contained in the parameter set'
 vertices_parameter_set = parameter_set.vertices
 
 
@@ -37,244 +41,86 @@ if K is None:
 
 
 rho, lmbd = compute_joint_spectral_radius(B, vertices_parameter_set, dim_x, dim_u, K)
-F = 5e-1*np.eye(dim_x)
-G = 5e-1*np.ones((dim_x, dim_u))
+F = np.array([ [0, -1/1.1], [0, 0]]) #[-1/(0.5), 0],
+G = 2*np.array([[0], [1]]) #[0],
 n = 1
 T = compute_contractive_polytope(n, lmbd, B, F, G, K, vertices_parameter_set)
 
 
 std_w = 1e-2
+horizon = 10
 W = np.hstack(build_hypercube(np.zeros(dim_x), 3 *  std_w))
 
 print(f'Joint spectral radius: {rho} - lambda: {lmbd}')
-# # def compute_H_Hc(vertices: np.ndarray, T: np.ndarray, K: np.ndarray, F: np.ndarray, G: np.ndarray) -> cp.Problem:
-# #     """
-# #     Note that the number of vertices is always fixed if we use hypercubes. This makes the problem more computationally efficient
-# #     so the number of vertices is not time varying
-# #     """
-# #     num_vertices = vertices.shape[0]
-# #     dalpha, dx = T.shape
-# #     dc = F.shape[0]
-# #     dim_u, dim_x = K.shape
-
-# #     vertices = vertices.reshape(num_vertices, dim_x, dim_x+dim_u)
-
-# #     matrices_H = [cp.Variable((dalpha, dalpha), nonneg=True) for j in range(num_vertices)]
-# #     Hc = cp.Variable((dc, dalpha), nonneg=True)
-# #     VParams = [cp.Parameter((vertices.shape[1:]), name=f'vertex_{j}') for j in range(num_vertices)]
 
 
-# #     objective = cp.sum(Hc)
-# #     constraints = [Hc @ T == F +  G @ K]
 
-# #     # # Constraints H
-# #     # for j in range(num_vertices):
-# #     #     Av, Bv = VParams[j][:, :dim_x], VParams[j][:, dim_x:]
-# #     #     constraints.append(matrices_H[j] @ T == T @ (Av + Bv @ K))
-# #     #     objective += cp.sum(matrices_H[j])
-    
-# #     problem = cp.Problem(cp.Minimize(objective), constraints)
-# #     return problem
-
-def compute_Hc(T: np.ndarray, K: np.ndarray, F: np.ndarray, G: np.ndarray) -> np.ndarray:
-    """
-    Compute matrix Hc
-    """
-    dalpha, dx = T.shape
-    dc = F.shape[0]
-
-    Hc = [cp.Variable((dalpha), nonneg=True) for i in range(dc)]
-    FGK = F + G @ K
-    for i in range(dc):
-        problem = cp.Problem(cp.Minimize(cp.sum(Hc[i])), [Hc[i] @ T == FGK[i]])
-        problem.solve()
-    
-
-    return np.stack([Hc[i].value for i in range(dc)])
-
-def get_H_problem(B: np.ndarray, vertices: np.ndarray, T: np.ndarray, K: np.ndarray, F: np.ndarray, G: np.ndarray) -> cp.Problem:
-    """
-    Note that the number of vertices is always fixed if we use hypercubes. This makes the problem more computationally efficient
-    so the number of vertices is not time varying
-    """
-    num_vertices = vertices.shape[0]
-    dalpha, dx = T.shape
-    dc = F.shape[0]
-    dim_u, dim_x = K.shape
-
-    vertices = vertices.reshape(num_vertices, dim_x, dim_x)
-
-    h = [[cp.Variable((dalpha), nonneg=True) for i in range(dalpha)] for j in range(num_vertices)]
-    VParams = [cp.Parameter((vertices.shape[1:]), name=f'vertex_{j}') for j in range(num_vertices)]
-
-    objective = 0
-    constraints= []
-
-
-    # Constraints H
-    for j in range(num_vertices):
-        Av = VParams[j]
-        phi = Av + B @ K
-        for i in range(dalpha):
-            objective += cp.sum(h[j][i])
-            constraints.append(h[j][i] @ T == T[i] @ phi)
-        
-    problem = cp.Problem(cp.Minimize(objective), constraints)
-
-    def solve_problem(vertices: np.ndarray) -> np.ndarray:
-        vertices = vertices.reshape(vertices.shape[0], dim_x, dim_x)
-        for j in range(vertices.shape[0]):
-            problem.param_dict[f'vertex_{j}'].value = vertices[j]
-        #problem = set_vertices_value(problem, vertices, dim_x, dim_u)
-        # vertices = vertices.reshape(num_vertices, dim_x, dim_x)
-        # for i in range(num_vertices):
-        #     VParams[i].value = vertices[j]
-
-        # res = [[problems[j][i].solve(verbose=False, warm_start=True) for i in range(dalpha)] for j in range(num_vertices)]
-        res = problem.solve(warm_start=True)
-        matrices = [np.stack([h[j][i].value for i in range(dalpha)]) for j in range(num_vertices)]
-        return matrices, res
-
-    return solve_problem
-
-# def set_vertices_value(problem: cp.Problem, vertices: np.ndarray, dim_x: int, dim_u: int) -> cp.Problem:
-#     vertices = vertices.reshape(vertices.shape[0], dim_x, dim_x)
-#     for j in range(vertices.shape[0]):
-#         problem.param_dict[f'vertex_{j}'].value = vertices[j]
-#     return problem
 
 Hc = compute_Hc(T, K, F, G)
-
 problem_H = get_H_problem(B, vertices_parameter_set, T, K, F, G)
-#problem_H = set_vertices_value(problem_H, vertices_parameter_set, dim_x, dim_u)
-import time
-start = time.time()
 H, _ = problem_H(vertices_parameter_set)
-
-# problem_H(vertices_parameter_set)
-# print(time.time() - start)
-# start = time.time()
-# problem_H(vertices_parameter_set)
-# print(time.time() - start)
-# # problem_H(vertices_parameter_set)
-# problem_H(vertices_parameter_set)
-# print(time.time() - start)
-
-
-def MPC_problem(
-        K: np.ndarray,
-        Hc: np.ndarray,
-        B: np.ndarray,
-        T: np.ndarray,
-        G: np.ndarray,
-        Q: np.ndarray,
-        R: np.ndarray,
-        horizon: int,
-        num_vertices: int) -> Callable[
-            [np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, List[np.ndarray]],
-            Tuple[np.ndarray, np.ndarray, float, float]
-            ]:
-
-
-    dim_x = Q.shape[0]
-    dim_u = R.shape[0]
-    v = cp.Variable((horizon, dim_u))
-    alpha = cp.Variable((horizon+1, T.shape[0]))
-    x = cp.Variable((horizon+1, dim_x))
-
-    A = cp.Parameter((dim_x, dim_x), name='A')
-    B = cp.Parameter((dim_x, dim_u), name='B')
-    P_sqrt = cp.Parameter((dim_x, dim_x), name='P_sqrt', PSD=True)
-    x0 = cp.Parameter((dim_x), name='x0')
-    wbar = cp.Parameter((T.shape[0]), name='wbar')
-
-    matrices_H = [cp.Parameter((T.shape[0], T.shape[0]), name=f'H_{i}') for i in range(num_vertices)]
-
-    constraints = [
-        x[0] == x0,
-        x[1:].T == (A + B @ K) @ x[:-1].T + B @ v.T,
-        Hc @ alpha[:-1].T + G @ v.T <= 1,
-        Hc @ alpha[-1] <= 1,
-        T @ x0 <= alpha[0]
-    ]
-    for i in range(num_vertices):
-        constraints.append(
-            matrices_H[i] @ alpha[-1] + wbar <= alpha[-1],
-        )
-        for k in range(horizon):
-            constraints.append(matrices_H[i] @ alpha[k] + T @ B @ v[k] + wbar <= alpha[k+1])
-
-    loss = cp.sum_squares(P_sqrt @ x[-1])
-    for i in range(horizon):
-        loss += cp.quad_form(x[i], Q) + cp.quad_form(v[i], R)
-
-    problem = cp.Problem(cp.Minimize(loss), constraints)
-
-
-    def solve_problem(
-            A: np.ndarray,
-            B: np.ndarray,
-            P: np.ndarray,
-            x0: np.ndarray,
-            wbar: np.ndarray,
-            H_matrices: List[np.ndarray]) -> Tuple[np.ndarray, np.ndarray, float, float]:
-        problem.param_dict['A'].value = A
-        problem.param_dict['B'].value = B
-        problem.param_dict['P_sqrt'].value = linalg.sqrtm(P)
-        problem.param_dict['x0'].value = x0
-        problem.param_dict['wbar'].value = wbar
-
-        for i in range(num_vertices):
-            problem.param_dict[f'H_{i}'].value = H_matrices[i]
-        
-        start = time.time()
-        res = problem.solve(solver=cp.MOSEK, warm_start=True, enforce_dpp=True, verbose=True)
-
-
-        elapsed_time = time.time() - start
-        
-        return v.value, alpha.value, res, elapsed_time
-
-    return solve_problem
-
-def solve_lyapunov(A: np.ndarray, B: np.ndarray, K: np.ndarray, Q: np.ndarray, R: np.ndarray) -> np.ndarray:
-    return linalg.solve_discrete_lyapunov(A + B @ K, Q + K.T @ R @ K)
-
-def compute_wbar(W_hs: np.ndarray, T: np.ndarray):
-    wbar = np.zeros((T.shape[0]))
-    w = cp.Variable(T.shape[1])
-    Tval = cp.Parameter((T.shape[1]))
-
-    problem = cp.Problem(cp.Maximize(Tval @ w), [W_hs[:,:-1] @ w + W_hs[:,-1] <= 0])
-
-    for i in range(T.shape[0]):
-        Tval.value = T[i]
-        wbar[i] = problem.solve(warm_start=True)
-
-    return wbar
-
-
-
-solve_mpc_problem = MPC_problem(K, Hc, B, T, G, np.eye(dim_x), np.eye(dim_u), 10, vertices_parameter_set.shape[0])
-
+solve_mpc_problem = MPC_problem(K, Hc, B, T, G, np.eye(dim_x), np.eye(dim_u), horizon, vertices_parameter_set.shape[0])
 
 wbar = compute_wbar(W, T)
 
 
-P = solve_lyapunov(A, B, K, np.eye(dim_x), np.eye(dim_u))
-x0 = np.random.standard_normal(size=(dim_x))
 
-v, alpha, res, time_elapsed = solve_mpc_problem(A, B, P, x0, wbar, H)
-print(v)
-print(alpha)
-print(res)
-print(time_elapsed)
-# start = time.time()
-# problem_H(vertices_parameter_set)
-# print(time.time() - start)
-# start = time.time()
-# problem_H(vertices_parameter_set)
-# print(time.time() - start)
-# # problem = compute_H_Hc(vertices_parameter_set, T, K, F, G)
-# # problem = set_vertices_value(problem, vertices_parameter_set, dim_x, dim_u)
-# # problem.solve(verbose=True, solver=cp.MOSEK, warm_start=False)
+
+TOTAL_HORIZON = 500
+x = np.zeros((TOTAL_HORIZON+1, dim_x))
+u = np.zeros((TOTAL_HORIZON, dim_u))
+epsilon = np.zeros(TOTAL_HORIZON)
+volume = np.zeros(TOTAL_HORIZON)
+const_eps = 40
+delta=1e-3
+const_eps2 = 1
+N_LS = 2
+
+x[0] = np.array([6,3])
+A_t = parameter_set.center.reshape((dim_x, dim_x))
+
+for t in range(TOTAL_HORIZON):
+    volume[t] = parameter_set.volume
+    print(f'[({t})] x_t: {x[t]} - A_t {A_t} - Contains A: {parameter_set.contains(A.flatten())} - Volume: {volume[t]} - {np.linalg.norm(A_t - A, ord="fro")}')
+    
+
+    P_t = solve_lyapunov(A_t, B, K, np.eye(dim_x), np.eye(dim_u))
+
+    v, alpha, res, time_elapsed = solve_mpc_problem(A_t, B, P_t, x[t], wbar, H)
+    if v is None:
+        raise Exception('Problem unfeasible')
+
+
+    u[t] = K@x[t] + v[0] + std_w * np.sqrt(dim_x) * np.random.standard_normal()
+    x[t+1] = A  @ x[t] + B @ u[t] + std_w * np.random.standard_normal(dim_x)
+
+    epsilon[t] = const_eps * (std_w ** 2) * (np.log(np.e/delta) + (dim_x+dim_u) * np.log(const_eps2 * std_w * dim_x * dim_u + (t + 1))) / (t + 1) ** 0.6
+
+
+    if t > N_LS:
+        #import pdb
+        #pdb.set_trace()
+        # LS Estimate
+        _X = x[:t+2, :].T
+        _U = u[:t+1, :].T
+        #ata = np.vstack((_X[:, :-1], _U))
+        #theta_t = (_X[:, 1:] @ np.linalg.pinv(data)).flatten()
+
+        theta_t = ((_X[:, 1:] - B @ _U) @ np.linalg.pinv(_X[:,:-1])).flatten()
+
+
+        delta_t = HyperRectangle.build_hypercube(theta_t, 2 * epsilon[t])
+        parameter_set = parameter_set.intersect(delta_t)
+        A_t = theta_t.reshape((dim_x, dim_x))
+
+        vertices_parameter_set = parameter_set.vertices
+        H, _ = problem_H(vertices_parameter_set)
+
+plt.plot(volume)
+plt.yscale('log')
+plt.grid()
+plt.show()
+
+plt.plot(x[:,0], x[:, 1])
+plt.grid()
+plt.show()
--- known_B/utils.py
+++ known_B/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cvxpy as cp
-from typing import Tuple, Union, Optional
+import time
+from typing import Tuple, Union, Optional, List, Callable
+import scipy.linalg as linalg
 
 
 def build_aligned_hypercube(A: np.ndarray, center: np.ndarray, half_side_length: float) -> Tuple[np.ndarray, np.ndarray]:
@@ -159,7 +161,24 @@ def compute_contractive_polytope(n: int, lmbd: float, B: np.ndarray, F: np.ndarr
 
     return np.vstack(T)
 
-def compute_H_Hc(vertices: np.ndarray, T: np.ndarray, K: np.ndarray, F: np.ndarray, G: np.ndarray) -> cp.Problem:
+def compute_Hc(T: np.ndarray, K: np.ndarray, F: np.ndarray, G: np.ndarray) -> np.ndarray:
+    """
+    Compute matrix Hc
+    """
+    dalpha, dx = T.shape
+    dc = F.shape[0]
+
+    Hc = [cp.Variable((dalpha), nonneg=True) for i in range(dc)]
+    FGK = F + G @ K
+    for i in range(dc):
+        problem = cp.Problem(cp.Minimize(cp.sum(Hc[i])), [Hc[i] @ T == FGK[i]])
+        problem.solve()
+    
+
+    return np.stack([Hc[i].value for i in range(dc)])
+
+
+def get_H_problem(B: np.ndarray, vertices: np.ndarray, T: np.ndarray, K: np.ndarray, F: np.ndarray, G: np.ndarray) -> cp.Problem:
     """
     Note that the number of vertices is always fixed if we use hypercubes. This makes the problem more computationally efficient
     so the number of vertices is not time varying
@@ -169,27 +188,129 @@ def compute_H_Hc(vertices: np.ndarray, T: np.ndarray, K: np.ndarray, F: np.ndarr
     dc = F.shape[0]
     dim_u, dim_x = K.shape
 
-    vertices = vertices.reshape(num_vertices, dim_x, dim_x+dim_u)
+    vertices = vertices.reshape(num_vertices, dim_x, dim_x)
 
-    matrices_H = [cp.Variable((dalpha, dalpha), nonneg=True) for j in range(num_vertices)]
-    Hc = cp.Variable((dc, dalpha), nonneg=True)
+    h = [[cp.Variable((dalpha), nonneg=True) for i in range(dalpha)] for j in range(num_vertices)]
     VParams = [cp.Parameter((vertices.shape[1:]), name=f'vertex_{j}') for j in range(num_vertices)]
 
+    objective = 0
+    constraints= []
 
-    objective = cp.sum(Hc)
-    constraints = [Hc @ T == F +  G @ K]
 
     # Constraints H
     for j in range(num_vertices):
-        Av, Bv = VParams[j][:, :dim_x], VParams[j][:, dim_x:]
-        constraints.append(matrices_H[j] @ T == T @ (Av + Bv @ K))
-        objective += cp.sum(matrices_H[j])
-    
+        Av = VParams[j]
+        phi = Av + B @ K
+        for i in range(dalpha):
+            objective += cp.sum(h[j][i])
+            constraints.append(h[j][i] @ T == T[i] @ phi)
+        
     problem = cp.Problem(cp.Minimize(objective), constraints)
-    return problem
 
-def set_vertices_value(problem: cp.Problem, vertices: np.ndarray, dim_x: int, dim_u: int) -> cp.Problem:
-    vertices = vertices.reshape(vertices.shape[0], dim_x, dim_x+dim_u)
-    for j in range(vertices.shape[0]):
-        problem.param_dict[f'vertex_{j}'].value = vertices[j]
-    return problem
+    def solve_problem(vertices: np.ndarray) -> np.ndarray:
+        vertices = vertices.reshape(vertices.shape[0], dim_x, dim_x)
+        for j in range(vertices.shape[0]):
+            problem.param_dict[f'vertex_{j}'].value = vertices[j]
+        #problem = set_vertices_value(problem, vertices, dim_x, dim_u)
+        # vertices = vertices.reshape(num_vertices, dim_x, dim_x)
+        # for i in range(num_vertices):
+        #     VParams[i].value = vertices[j]
+
+        # res = [[problems[j][i].solve(verbose=False, warm_start=True) for i in range(dalpha)] for j in range(num_vertices)]
+        res = problem.solve(warm_start=True)
+        matrices = [np.stack([h[j][i].value for i in range(dalpha)]) for j in range(num_vertices)]
+        return matrices, res
+
+    return solve_problem
+
+def MPC_problem(
+        K: np.ndarray,
+        Hc: np.ndarray,
+        B: np.ndarray,
+        T: np.ndarray,
+        G: np.ndarray,
+        Q: np.ndarray,
+        R: np.ndarray,
+        horizon: int,
+        num_vertices: int) -> Callable[
+            [np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, List[np.ndarray]],
+            Tuple[np.ndarray, np.ndarray, float, float]
+            ]:
+
+
+    dim_x = Q.shape[0]
+    dim_u = R.shape[0]
+    v = cp.Variable((horizon, dim_u))
+    alpha = cp.Variable((horizon+1, T.shape[0]))
+    x = cp.Variable((horizon+1, dim_x))
+
+    A = cp.Parameter((dim_x, dim_x), name='A')
+    B = cp.Parameter((dim_x, dim_u), name='B')
+    P_sqrt = cp.Parameter((dim_x, dim_x), name='P_sqrt', PSD=True)
+    x0 = cp.Parameter((dim_x), name='x0')
+    wbar = cp.Parameter((T.shape[0]), name='wbar')
+
+    matrices_H = [cp.Parameter((T.shape[0], T.shape[0]), name=f'H_{i}') for i in range(num_vertices)]
+
+    constraints = [
+        x[0] == x0,
+        x[1:].T == (A + B @ K) @ x[:-1].T + B @ v.T,
+        Hc @ alpha[:-1].T + G @ v.T <= 1,
+        Hc @ alpha[-1] <= 1,
+        T @ x0 <= alpha[0]
+    ]
+    for i in range(num_vertices):
+        constraints.append(
+            matrices_H[i] @ alpha[-1] + wbar <= alpha[-1],
+        )
+        for k in range(horizon):
+            constraints.append(matrices_H[i] @ alpha[k] + T @ B @ v[k] + wbar <= alpha[k+1])
+
+    loss = cp.sum_squares(P_sqrt @ x[-1])
+    for i in range(horizon):
+        loss += cp.quad_form(x[i], Q) + cp.quad_form(v[i], R)
+
+    problem = cp.Problem(cp.Minimize(loss), constraints)
+
+
+    def solve_problem(
+            A: np.ndarray,
+            B: np.ndarray,
+            P: np.ndarray,
+            x0: np.ndarray,
+            wbar: np.ndarray,
+            H_matrices: List[np.ndarray]) -> Tuple[np.ndarray, np.ndarray, float, float]:
+        problem.param_dict['A'].value = A
+        problem.param_dict['B'].value = B
+        problem.param_dict['P_sqrt'].value = linalg.sqrtm(P)
+        problem.param_dict['x0'].value = x0
+        problem.param_dict['wbar'].value = wbar
+
+        for i in range(num_vertices):
+            problem.param_dict[f'H_{i}'].value = H_matrices[i]
+        
+        start = time.time()
+        res = problem.solve(solver=cp.MOSEK, warm_start=True, enforce_dpp=True, verbose=False)
+
+
+        elapsed_time = time.time() - start
+        
+        return v.value, alpha.value, res, elapsed_time
+
+    return solve_problem
+
+def solve_lyapunov(A: np.ndarray, B: np.ndarray, K: np.ndarray, Q: np.ndarray, R: np.ndarray) -> np.ndarray:
+    return linalg.solve_discrete_lyapunov(A + B @ K, Q + K.T @ R @ K)
+
+def compute_wbar(W_hs: np.ndarray, T: np.ndarray):
+    wbar = np.zeros((T.shape[0]))
+    w = cp.Variable(T.shape[1])
+    Tval = cp.Parameter((T.shape[1]))
+
+    problem = cp.Problem(cp.Maximize(Tval @ w), [W_hs[:,:-1] @ w + W_hs[:,-1] <= 0])
+
+    for i in range(T.shape[0]):
+        Tval.value = T[i]
+        wbar[i] = problem.solve(warm_start=True)
+
+    return wbar
